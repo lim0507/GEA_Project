1b2b4939f6124ededd4a8c7d200118fea8ddb396
lim0507 <ljs417848@gmail.com>
2025-09-18 15:29:17 +0900

    9.18

---
+++ GEA/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpPower = 5f;
+    public float gravity = -9.81f;
+    public CinemachineVirtualCamera virtualCam;
+    public float rotationSpeed = 10f;
+    private CinemachinePOV pov;
+    private CharacterController controller;
+    private Vector3 velocity;
+    public bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        pov = virtualCam.GetCinemachineComponent<CinemachinePOV>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        isGrounded = controller.isGrounded;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 5f;
+            virtualCam.m_Lens.FieldOfView = 80f;
+        }
+        
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 5f;
+            virtualCam.m_Lens.FieldOfView = 60f;
+        }
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 camForward = virtualCam.transform.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+
+        Vector3 camRight = virtualCam.transform.right;
+        camRight.y = 0;
+        camRight.Normalize();
+
+        Vector3 move = (camForward * z + camRight * x).normalized;
+        controller.Move(move * speed * Time.deltaTime);
+
+        float cameraYaw = pov.m_HorizontalAxis.Value;
+        Quaternion targerRot = Quaternion.Euler(0f, cameraYaw, 0f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targerRot, rotationSpeed * Time.deltaTime);
+
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity.y = jumpPower;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        
+    }
+}
